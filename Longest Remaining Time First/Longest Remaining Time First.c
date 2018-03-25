74a74cf5152963a3bd51c6eecf1defcedd90831a
Aman Goel <amantyphoon2013@outlook.com>
2018-03-25 20:52:51 +0530

    Update Longest Remaining Time First.c

--- Longest Remaining Time First/Longest Remaining Time First.c
+++ Longest Remaining Time First/Longest Remaining Time First.c
@@ -4,49 +4,71 @@
 typedef struct Process{
 	int Process_Id;
 	int Burst_Time;
+	int Finish_Time;
+	bool Completed;
 }Process;
 typedef struct Gantt{
 	int Process_Id;
 	int Start_Time;
 	int Finish_Time;
 }Gantt;
-Process Process_Queue[]={{2132,2},{2453,4},{2102,8}};
-Gantt Gantt_Chart[14];
-bool completed[3]={false,false,false};
-int Sequence[8];
-int count=0;
-float Turnaround_Time,Waiting_Time;
+Process Process_Queue[]={{2132,2,0,false},{2453,4,0,false},{2102,8,0,false}};
+Gantt Gantt_Chart[15];
+int Time_Value=0;
+int Turnaround_Time[3],Waiting_Time[3];
 
+void Display_Sequence();
+int Display_Avg();
 int main(){
-	int i,gi=0;
+	int i,count=0;
 	Process *t,*pq;
-	Gantt *g;
+	Gantt *g=Gantt_Chart;
 	int artime=0;
 	while(count<3){
-		*g=Gantt_Chart[gi];
-		*t=Process_Queue[0];
-		*pq=Process_Queue[1];
+		t=Process_Queue;
+		pq=Process_Queue;
 		for(i=1;i<3;i++){
-			if(!completed[i]){
+			*pq++;
+			if(!t->Completed){
 				if(pq->Burst_Time>t->Burst_Time)t=pq;
 				else if(pq->Burst_Time==t->Burst_Time)
 					if(pq->Process_Id<t->Process_Id)t=pq;
 				else ;				
 			}
-			*pq++;
 		}
+		printf("%d\t%d\t",t->Burst_Time,t->Process_Id);
 		g->Process_Id=t->Process_Id;
 		g->Start_Time=artime;
-		g->Start_Time=artime+1;
+		g->Finish_Time=artime+1;
+		*g++;
 		t->Burst_Time-=1;
-		if(t->Burst_Time==0)completed[t->Process_Id]=true;
+		Time_Value++;
+		if(t->Burst_Time<=0){
+			t->Finish_Time=Time_Value;
+			t->Completed=true;
+			count++;
+		}
+		printf("%d %d %d\n",g->Process_Id,g->Start_Time=artime,g->Finish_Time);
 	}
-	*g=Gantt_Chart[0];
-	for(i=0;i<15;i++){
+	printf("Completed");
+	Display_Sequence();
+	Display_Avg();
+	return 0;
+}
+int Display_Avg(){
+	
+}
+void Display_Sequence(){
+	Gantt *g;int i;
+	g=Gantt_Chart;
+	for(i=0;i<Time_Value;i++){
 		printf("| P%d | ",g->Process_Id);
+		*g++;
 	}
-	*g=Gantt_Chart[0];
-	for(i=0;i<15;i++){
-		printf("&d  &d",g->Start_Time,g->Finish_Time);
+	printf("\n");
+	g=Gantt_Chart;
+	for(i=0;i<Time_Value;i++){
+		printf("%d  %d",g->Start_Time,g->Finish_Time);
+		*g++;
 	}
 }
