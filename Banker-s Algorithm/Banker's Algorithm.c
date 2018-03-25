3b2748ca09336f270c7f761101723aa3a039d06c
Aman Goel <amantyphoon2013@outlook.com>
2018-03-25 20:07:47 +0530

    Update Banker's Algorithm.c

--- Banker-s Algorithm/Banker's Algorithm.c
+++ Banker-s Algorithm/Banker's Algorithm.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int n=3,m=3,i=0;
 
-int Resource_Available[][3]={{8,4,2},{3,0,0},{1,2,2}};
+int Resource_Available[3]={3,2,2};
 int Resource_Allocated[][3]={{0,0,1},{3,2,0},{2,1,1}};
 int Process_Maximum_Need[][3]={{8,4,3},{6,2,0},{3,3,3}};
 int Process_Current_Need[3][3];
@@ -9,8 +9,9 @@ int Process_Current_Need[3][3];
 int *RAv,*RAl,*PMx,*PNd;
 
 void Display_Table();
-int Request_Algo(int,int);
-int Check_Algo(int,int);
+int Request_Algo(int,int *);
+int Check_Algo(int,int *);
+void Calculate_Need();
 
 int main(){
 	int i,j;
@@ -19,31 +20,37 @@ int main(){
 	int a[]={0,0,2};
 	int *R=a;
 	Check_Algo(0,R);
+	Display_Table();
 	int b[]={2,0,0};
 	R=b;
-	//Check_Algo(1,R);
+	Check_Algo(1,R);
+	Display_Table();
 }
 
 int Request_Algo(int pid,int *R){
-	//1
 	int i;
-	RAv=Resource_Available[pid];
+	RAv=Resource_Available;
 	RAl=Resource_Allocated[pid];
 	PMx=Process_Maximum_Need[pid];
 	PNd=Process_Current_Need[pid];
 	for(i=0;i<m;i++){
-		if(*(R+i)<*(PNd+i))return 0;
-		if(*(R+i)<*(RAv+i))return 0;
+		if(*(R+i)>*(PNd+i))return 0;
+		if(*(R+i)>*(RAv+i))return 0;
 	}
-	//2
-	printf("Process %d is possible to continue\n",pid);
+	printf("Process %d is possible for Request: ",pid);
+	printf("<");for(i=0;i<3;i++)printf("%d,",*(R+i));printf(">\n");
 	for(i=0;i<m;i++){
 		*(RAl+i)+=*(R+i);
 		*(RAv+i)-=*(R+i);
-		*(PNd+pid+i)+=*(R+i);
+		*(PNd+i)-=*(R+i);
 	}
 	return 1;
 }
+
+int Safety_Algo(int pid,int *R){
+	
+}
+
 int Check_Algo(int pid,int *R){
 	if(Request_Algo(pid,R)==1){
 		printf("Request Satisfied\nChecking Safety\n");
@@ -52,6 +59,9 @@ int Check_Algo(int pid,int *R){
 		printf("Request Not Satisfied\n");
 		return 0;
 	}
+}
+int Create_Safe_Sequence(){
+	
 }
 void Calculate_Need(){
 	int pid,i,j;
@@ -66,9 +76,10 @@ void Calculate_Need(){
 }
 void Display_Table(){
 	int i,j;
-	printf("Process\tMaximum\t\tAllocated\tNeed\n");
+	printf("\nProcess\tMaximum\t\tAllocated\tNeed\t\tAvailable\n");
   	for(j=0;j<n;j++){
 		RAl=Resource_Allocated[j];
+		RAv=Resource_Available;
 		PMx=Process_Maximum_Need[j];
 		PNd=Process_Current_Need[j];
 		printf("P%d\t",j);
@@ -83,6 +94,8 @@ void Display_Table(){
 		for(i=0;i<m;i++){
 			printf("%d ",*(PNd+i));
 		}
+		printf("\t\t");
+		printf("R%d: %d ",j,*(RAv+j));
 		printf("\n");
 	}
 	printf("\n");
